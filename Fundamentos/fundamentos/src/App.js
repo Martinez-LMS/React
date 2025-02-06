f6a9c09a0a34d343a933ea66c74fea801d07c3bb
Martinez-LMS <leo.ooosla@gmail.com>
2025-02-05 22:05:55 -0300

    .

---
+++ Fundamentos/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import PrimeiroComponent from './Components/PrimeiroComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos de React</h1>
+      <PrimeiroComponent /> {/* Adicione o componente aqui */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Fundamentos/fundamentos/src/Components/PrimeiroComponent.js
@@ -0,0 +1,10 @@
+const PrimeiroComponent = () => {
+    return ()  => {
+        <div>
+        <h1>Primeiro Componente</h1>
+        <h2>Exemplo de um componente React</h2>
+        </div>
+    }
+}
+
+export default PrimeiroComponent;
