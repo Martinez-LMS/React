b1dd5e3c20557a0c0bb8a93032de652033a3ac9c
Martinez-LMS <leo.ooosla@gmail.com>
2025-08-07 21:44:55 -0300

    foi

---
+++ Fundamentos/meu-app/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançado React</h1>
+      <div>
+        <img
+          src="/img/caixa-de-presente-de-renderizacao-3d-com-pacote-de-presente-de-fita.jpg"
+          alt="caixa presente"
+          width="300"
+        />
+
+        
+      </div>
+      
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
