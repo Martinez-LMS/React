1045377261365ffd03f04a5aa4370f04e84fa061
Martinez-LMS <leo.ooosla@gmail.com>
2025-08-13 21:30:53 -0300

    key react

--- Fundamentos/meu-app/src/components/ListRender.js
+++ Fundamentos/meu-app/src/components/ListRender.js
@@ -2,14 +2,37 @@ import {useState} from 'react'
 
 const ListRender = () => {
 
+
+    const [user, setUsers] = useState([
+      {name: "Matheus", age: 30},
+      {name: "Pedro", age: 25},
+      {name: "Leo", age: 20},
+    ]);
+
+    const deleteRandom = () => {
+      const randomNumber = Math.floor(Math.random() * 4);
+      console.log(randomNumber);
+      setUsers((prevUsers) => {
+        return prevUsers.filter((user, i) => i !== randomNumber);
+      });
+    };
+
+    
     const [list] = useState(["Matheus-", "pedro-", "leo"]);
   return (
     <div>
         <ul>
-            {list.map((item)=>(
-                <li>{list}</li>
+            {list.map((item, i)=>(
+                <li> key={i}{list}</li>
             ))}
         </ul>
+        <ul>
+          {user.map((user) => (
+            <li key={useState.id}>
+              {user.name} - {user.age}
+              </li>))}
+        </ul>
+        <button onClick={deleteRandom}>Deletar random user</button>
     </div>
   )
 }
